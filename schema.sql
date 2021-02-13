28b87ca410931dba28cef665b228a8588abe500c
Ben Malamud-Roam <bmalamudroam@gmail.com>
2021-02-13 15:12:23 -0800

    created tables in schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,22 @@
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS REPLACE_THIS;
 
-CREATE DATABASE test;
+CREATE DATABASE REPLACE_THIS;
 
-USE test;
+USE REPLACE_THIS;
 
-CREATE TABLE items (
-  id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
-  PRIMARY KEY (ID)
+CREATE TABLE users (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  username varchar(80) NOT NULL,
+  highscore INT NOT NULL DEFAULT '0',
+  cart_style varchar (20) NOT NULL DEFAULT 'white',
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE scores (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  score INT NOT NULL default 0,
+  user_id INT UNSIGNED NOT NULL REFERENCES users(id);
+)
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
